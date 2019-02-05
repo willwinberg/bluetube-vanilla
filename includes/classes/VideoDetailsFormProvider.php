1d2288603afd1748ff95050331114140e33f8bca
William <wtpwinberg@gmail.com>
2019-02-04 18:04:01 -0800

    description input textarea bug

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -3,10 +3,16 @@ class VideoDetailsFormProvider {
 
     public function createUploadForm() {
         $fileInput = $this->createFileInput();
+        $titleInput = $this->createTitleInput();
+        $descriptionInput = $this->createDescriptionInput();
+        $privacyInput = $this->createPrivacyInput();
 
         return ("
             <form action='processing.php' method='POST'>
                 $fileInput
+                $titleInput
+                $descriptionInput
+                $privacyInput
             </form>
         ");
     }
@@ -22,6 +28,38 @@ class VideoDetailsFormProvider {
                     name='fileInput'
                     required
                 >
+            </div>
+        ");
+    }
+
+    private function createTitleInput() {
+        return ("
+            <div class='form-group'>
+                <input class='form-control' type='text' placeholder='Title' name='titleInput'>
+            </div>
+        ");
+    }
+
+    private function createDescriptionInput() {
+        return ("
+            <div class='form-group'>
+                <textarea
+                    class='form-control'
+                    placeholder='Description'
+                    name='descriptionInput'
+                    rows='3'
+                ></textarea>
+            </div>
+        ");
+    }
+
+    private function createPrivacyInput() {
+        return ("
+            <div class='form-group'>
+                <select class='form-control' name='privacyInput'>
+                    <option value='0'>Private</option>
+                    <option value='1'>Public</option>
+                </select>
             </div>
         ");
     }
--- upload.php
+++ upload.php
@@ -4,7 +4,10 @@ require_once("includes/classes/VideoDetailsFormProvider.php");
 ?>
 
 <div class="column">
-
+   <?php
+    $formProvider = new VideoDetailsFormProvider();
+    echo $formProvider->createUploadForm();
+    ?>
 </div>
 
 <?php require_once("includes/footer.php"); ?>
