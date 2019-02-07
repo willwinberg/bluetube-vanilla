6220caa0a5ce8da8c52eb98aa91220da3daa6e5e
William <wtpwinberg@gmail.com>
2019-02-07 04:30:38 -0800

    Video playing on watch page

---
+++ includes/classes/Video.php
@@ -0,0 +1,78 @@
+<?php
+
+class Video {
+
+   private $dbConnection;
+   private $user;
+
+   public 
+      $id,
+      $tile,
+      $description,
+      $privacy,
+      $category,
+      $views,
+      $duration,
+      $filePath,
+      $uploadedBy,
+      $uploadedDate
+   ;
+
+
+   public function __construct($dbConnection, $input, $user) {
+      $this->dbConnection = $dbConnection;
+      $this->user = $user;
+
+      if (is_array($input)) {
+         $video = $input;
+      } else {
+         $query = $this->dbConnection->prepare(
+            "SELECT * FROM videos WHERE id = :id"
+         );
+         $query->bindParam(":id", $input);
+         $query->execute();
+
+         $video = $query->fetch(PDO::FETCH_ASSOC);
+      }
+
+      $this->id = $video["id"];
+      $this->title = $video["title"];
+      $this->description = $video["description"];
+      $this->privacy = $video["privacy"];
+      $this->category = $video["category"];
+      $this->views = $video["views"];
+      $this->duration = $video["duration"];
+      $this->filePath = $video["filePath"];
+      $this->uploadedBy = $video["uploadedBy"];
+      $this->uploadDate = $video["uploadDate"];  
+   }
+
+   public function generatePlayer($setAutoplay) {
+      $autoplay = $setAutoplay ? "autoplay" : "";
+
+      return ("
+         <video class='videoPlayer' controls $autoplay>
+            <source src='$this->filePath' type='video/mp4'>
+                  Your browser does not support the video tag
+         </video>
+      ");
+    }
+   
+   public function getUploadDate() {
+      return date("M j, Y", strtotime($this->date));
+   }
+
+   public function getTimeStamp() {
+      return date("M jS, Y", strtotime($this->date));
+   }
+
+   public function incrementViews() {
+      $query = $this->dbConnection->prepare(
+         "UPDATE videos SET views=views+1 WHERE id=:id"
+      );
+      $query->bindParam(":id", $this->id);
+      $query->execute();
+
+      $this->views++;
+   }
+}
--- watch.php
+++ watch.php
@@ -1,12 +1,23 @@
 <?php 
 require_once("includes/header.php"); 
+require_once("includes/classes/Video.php"); 
 
 if (!isset($_GET["id"])) {
     echo "Video URL missing";
     exit();
+   //  header("Location: index.php");
 }
+
+$video = new Video($dbConnection, $_GET["id"], $user);
+$video->incrementViews();
+echo $video->views;
 ?>
 
+<div class="watchLeft">
+   <?php echo $video->generatePlayer(true); ?>
+</div>
+<div class="suggestions">
 
+</div>
 
 <?php require_once("includes/footer.php"); ?>
