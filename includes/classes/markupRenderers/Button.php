e92c217e903e53b0141da2e440a8f04e0110d4b5
William <wtpwinberg@gmail.com>
2019-02-10 03:38:00 -0800

    edit video button and generic hyperlink button created

--- includes/classes/markupRenderers/Button.php
+++ includes/classes/markupRenderers/Button.php
@@ -21,6 +21,20 @@ class Button {
       ";
    }
 
+   public static function hyperlink($text, $link, $class, $src) {
+      $val = $text || $class;
+      $image = $src ? "<img src='$src' name='$val' alt='$val'>" : "";
+
+      return "
+         <a href='$link'>
+            <button class='$class'>
+               $image
+               <span class='text'>$text</span>
+            </button>
+         </a>
+      ";
+    }
+
    public static function userProfileButton($username, $image) {
       $link = "profile.php?username=$username";
 
@@ -31,7 +45,18 @@ class Button {
       ";
    }
 
-   public static function subscribeButton($dbConnection, $uploader, $subscriber) {
+   public static function editVideoButton($videoId) {
+      $link = "editVideo.php?videoId=$videoId";
+      $button = Button::hyperlink("EDIT VIDEO", $link, "edit button", NULL);
+
+      return "
+         <div class='editVideoButtonContainer'>
+            $button
+         </div>
+      ";
+    }
+
+   public static function subscribeButton($uploader, $subscriber) {
       $subscriptions = $subscriber->getSubscriptionsArray($uploader->username);
       $isSubscribed = in_array($subscriber->username);
       $text = $isSubscribedTo ? "SUBSCRIBED" : "SUBSCRIBE";
