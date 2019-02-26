10aa92b15aa8240c60bb0fb228e70b764b4c4e94
William <wtpwinberg@gmail.com>
2019-02-25 16:28:25 -0800

    changed editVideo to show error message when submitted woth no change

--- editVideo.php
+++ editVideo.php
@@ -34,7 +34,7 @@ if (!isset($_GET["videoId"])) {
 if (isset($_GET["success"])) $message = Success::$upload;
 
 $noChanges = isset($_POST["editVideo"])
-   && empty(array_intersect($_POST, $video->getDetailsArray()));
+   && array_diff_assoc($_POST, $video->getDetailsArray()) > 1;
 
 if ($noChanges && isset($_POST["editVideo"])) {
    $message = Error::$noChanges;
